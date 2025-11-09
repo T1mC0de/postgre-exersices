1e57d8d72e41b5d49e44347e61e632f962c86fd6
Timofei Sukhobok <tmsukhobok@edu.hse.ru>
2025-11-10 00:29:31 +0300

    initial commit with first task

---
+++ runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SQL_FILE=$1
+
+FULL_SQL_PATH="$(realpath "$SQL_FILE")"
+SQL_DIR="$(dirname "$FULL_SQL_PATH")"
+EXERCISES_DIR="$(dirname "$SQL_DIR")"
+RESULT_DIR="$EXERCISES_DIR/results"
+RESULT_FILE="$RESULT_DIR/$(basename "${SQL_FILE%.sql}")_result.txt"
+
+mkdir -p "$RESULT_DIR"
+
+psql -d exercises -f "$SQL_FILE" > "$RESULT_FILE"
+
+echo "✅ Готово! Результат сохранен"
